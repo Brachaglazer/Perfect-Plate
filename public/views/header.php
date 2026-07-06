7f4e1dfb71d565cf98e779a5e95c87e6bda78c1d
Brachaglazer <brachaglazer1@gmail.com>
2026-07-06 00:55:16 -0400

    move files and update forms styling

---
+++ public/views/header.php
@@ -0,0 +1,11 @@
+<HTML>
+<HEAD>
+<LINK rel="stylesheet" href="../css/styles.css" type="text/css" />
+</HEAD>
+<BODY>
+<img src="../images/logo.png" alt="PerfectPlate Logo" class="logo">
+<?php echo "<a href='signUpForm.html'>Sign Up</a>"; ?>
+<?php include "menu.php"; ?>
+
+<HR />
+
