e88e35a8f8a32985bac82f4c7d37782b36cb5139
mstern2321 <mstern2321@gmail.com>
2026-07-21 02:03:33 -0400

    Update stylesheet and script paths in header.php

--- public/views/header.php
+++ public/views/header.php
@@ -5,10 +5,10 @@
     <title>PerfectPlate</title>
 
     <link rel="stylesheet" href="../css/styles.css" type="text/css">
-    <link rel="stylesheet" href="css/slimmenu.min.css" type="text/css">
+    <link rel="stylesheet" href="../css/slimmenu.min.css" type="text/css">
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="jquery.slimmenu.min.js"></script>
+    <script src="../css/jquery.slimmenu.min.js"></script>
     <script src="responsiveslides.min.js"></script>
 </head>
 
