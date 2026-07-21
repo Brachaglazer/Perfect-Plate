a1df1858e6cc2abf6864febdeeaa768fc9440d09
mstern2321 <mstern2321@gmail.com>
2026-07-21 01:17:30 -0400

    add slimmenu class

--- public/views/menu.php
+++ public/views/menu.php
@@ -1,12 +1,13 @@
 
 <nav>
-    <a href="index.php">Home</a>
-    <a href="second.php">About Us</a>
-    <a href="third.php">Grocery List</a>
-    <a href="fourth.php">Dietary Restrictions</a>
-    <a href="fifth.php">Weight Loss</a>
-    <a href="third.php">Family Meals</a>
-    <a href="sixth.php">My Pantry</a>
-
+    <ul id="navigation" class="slimmenu">
+        <li></li><a href="index.php">Home</a></li>
+        <li><a href="second.php">About Us</a></li>
+        <li><a href="third.php">Grocery List</a></li>
+        <li><a href="fourth.php">Dietary Restrictions</a></li>
+        <li><a href="fifth.php">Weight Loss</a></li>
+        <li><a href="third.php">Family Meals</a></li>
+        <li><a href="sixth.php">My Pantry</a></li>
+    </ul>
 
 </nav>
