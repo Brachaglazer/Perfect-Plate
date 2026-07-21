0b01857fcbd126e43d6e43d0ff1f10d17ff26e1c
mstern2321 <mstern2321@gmail.com>
2026-07-21 01:50:55 -0400

    Update menu.php

--- public/views/menu.php
+++ public/views/menu.php
@@ -1,7 +1,7 @@
 
 <nav>
     <ul id="navigation" class="slimmenu">
-        <li></li><a href="index.php">Home</a></li>
+        <li><a href="index.php">Home</a></li>
         <li><a href="second.php">About Us</a></li>
         <li><a href="third.php">Grocery List</a></li>
         <li><a href="fourth.php">Dietary Restrictions</a></li>
